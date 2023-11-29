50b259414b8c66f96a05d8b538e36a40092ba248
KevalGosrani <149711105+KevalGosrani@users.noreply.github.com>
2023-11-30 01:24:34 +0530

    Add files via upload

---
+++ DS_04.R
@@ -0,0 +1,30 @@
+##Installing Required Packages
+install.packages ("tidytext")
+install.packages ("tidyverse") 
+install.packages ("wordcloud")                 
+install.packages ("ggplot2")
+install.packages ("textdata")
+#Accessing Library
+library(tidyverse)
+library (tidytext)
+library (wordcloud)
+#Loading Data Sets 
+data=Twitter 
+data
+#Analysis 
+data=data %>%
+  select (Text,Sentiment)%>%
+  mutate (Text=tolower (Text))%>% 
+  unnest_tokens (word,Text)
+#Processing
+data <- data %>%
+  inner_join (.,get_sentiments("afinn"), by = "word") %%
+  group_by (Sentiment) %>%
+  summarize (Sentiment_score = sum(value))
+#Visualisation
+ggplot2(data, aes(x = Sentiment, y = Sentiment_score)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  labs(title = "Sentiment Analysis", X = "Sentiment", y = "Sentiment Score")
+
+
+
